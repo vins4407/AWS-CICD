9e6dcef68ce87e4b9237ab87a66cb47739221729
vins4407 <vkesherwani000@gmail.com>
2023-07-09 22:07:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AWS-CICD using CodePopeline, BeanStalk And Github !
         </p>
         <a
           className="App-link"
